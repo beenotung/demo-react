616a71ab1af3dfe76bfd87927da204e6132dfce4
Beeno Tung <aabbcc1241@yahoo.com.hk>
2020-07-03 09:17:58 +0800

    make stateful component with react class

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@ionic/core/css/core.css';
+import '@ionic/core/css/ionic.bundle.css';
+import {
+  IonApp,
+  IonButton,
+  IonCard,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonCardContent,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/react';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    counter: 0,
+  };
+  render() {
+    return (
+      <IonApp>
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Home Page</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent>
+          <IonCard>
+            <IonCardHeader>
+              <IonCardSubtitle>Welcome to Ionic</IonCardSubtitle>
+              <IonCardTitle>Running on React</IonCardTitle>
+            </IonCardHeader>
+            <IonCardContent>
+              <div className='App-logo-container'>
+                <img src={logo} className="App-logo" alt="ionic logo" />
+                The logo path: {logo}
+              </div>
+              <p>Counter: {this.state.counter}</p>
+              <IonButton onClick={() => this.increment()}>Increment</IonButton>
+            </IonCardContent>
+          </IonCard>
+        </IonContent>
+      </IonApp>
+    );
+  }
+
+  increment() {
+    this.setState({
+      counter: this.state.counter + 1,
+    });
+  }
 }
 
 export default App;
